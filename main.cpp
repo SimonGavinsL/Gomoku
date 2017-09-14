6672ad602d827864e4bb3c9878c5463b9fcec69c
Simon Gavins Lee <simongavinslee0013@icloud.com>
2017-09-14 16:00:31 +0800

    update pad

--- main.cpp
+++ main.cpp
@@ -11,28 +11,30 @@
 using namespace std;
 
 int main(int argc, const char * argv[]) {
-    cout << "Welcome to Gomoku!" << endl;
+    cout << "Welcome to Gomoku!" << endl << "Enter the location to place!" << endl;
     char pad[15][15];
-    bool flag = false, player = true;
+    bool player = true;
     int x, y;
     
     for(int ix = 0; ix < 15; ++ix)
         for(int iy = 0; iy < 15; ++iy)
             pad[ix][iy] = '.';
     
-    while(!flag){
-        //交换棋权
+    while(true){
+        //输出棋盘
+        cout << endl << " \t1 2 3 4 5 6 7 8 9 101112131415" << endl;
         for(int ix = 0; ix < 15; ++ix){
-            cout << endl;
+            cout << ix + 1 << '\t';
             for(int iy = 0; iy < 15; ++iy)
                 cout << pad[ix][iy] << ' ';
         }
-        player = !player;
+        
+        player = !player;   //交换棋权
         
         
         //落子及判断
         while(true){
-            cout << endl << "Player " << (player ? '2':'1') << ": ";
+            cout << endl << endl << "Player " << (player ? 'o':'*') << ": ";
             cin >> x >> y;
             if(pad[x-1][y-1] == '.'){
                 pad[x-1][y-1] = player ? 'o':'*';
@@ -53,12 +55,14 @@ int main(int argc, const char * argv[]) {
         for(int ix = x - 1, iy = y - 1; pad[ix][iy] == (player ? 'o':'*') && ix <= 14; ++ix)
             ++count1;
         if(count1 >= 6){
+            //因为自己这一颗棋遍历时被计了2次
+            cout << endl << " \t1 2 3 4 5 6 7 8 9 101112131415" << endl;
             for(int ix = 0; ix < 15; ++ix){
-                cout << endl;
+                cout << ix + 1 << '\t';
                 for(int iy = 0; iy < 15; ++iy)
                     cout << pad[ix][iy] << ' ';
             }
-            cout << endl << "Player " << (player ? '2':'1') << " wins!" << endl;
+            cout << endl << "Player " << (player ? 'o':'*') << " wins!" << endl;
             break;
         }
         
@@ -69,12 +73,13 @@ int main(int argc, const char * argv[]) {
         for(int ix = x - 1, iy = y - 1; pad[ix][iy] == (player ? 'o':'*') && iy <= 14; ++iy)
             ++count2;
         if(count2 >= 6){
+            cout << endl << " \t1 2 3 4 5 6 7 8 9 101112131415" << endl;
             for(int ix = 0; ix < 15; ++ix){
-                cout << endl;
+                cout << ix + 1 << '\t';
                 for(int iy = 0; iy < 15; ++iy)
                     cout << pad[ix][iy] << ' ';
             }
-            cout << endl << "Player " << (player ? '2':'1') << " wins!" << endl;
+            cout << endl << "Player " << (player ? 'o':'*') << " wins!" << endl;
             break;
         }
         
@@ -91,12 +96,13 @@ int main(int argc, const char * argv[]) {
             ++iy;
         }
         if(count3 >= 6){
+            cout << endl << " \t1 2 3 4 5 6 7 8 9 101112131415" << endl;
             for(int ix = 0; ix < 15; ++ix){
-                cout << endl;
+                cout << ix + 1 << '\t';
                 for(int iy = 0; iy < 15; ++iy)
                     cout << pad[ix][iy] << ' ';
             }
-            cout << endl << "Player " << (player ? '2':'1') << " wins!" << endl;
+            cout << endl << "Player " << (player ? 'o':'*') << " wins!" << endl;
             break;
         }
         
@@ -113,12 +119,13 @@ int main(int argc, const char * argv[]) {
             ++iy;
         }
         if(count4 >= 6){
+            cout << endl << " \t1 2 3 4 5 6 7 8 9 101112131415" << endl;
             for(int ix = 0; ix < 15; ++ix){
-                cout << endl;
+                cout << ix + 1 << '\t';
                 for(int iy = 0; iy < 15; ++iy)
                     cout << pad[ix][iy] << ' ';
             }
-            cout << endl << "Player " << (player ? '2':'1') << " wins!" << endl;
+            cout << endl << "Player " << (player ? 'o':'*') << " wins!" << endl;
             break;
         }
         
